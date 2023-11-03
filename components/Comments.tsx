5d3a89383444717a0d0dd4f9a73e98420b835dde
Matt Wicks <matt.wicks@gmail.com>
2023-11-03 16:35:06 +1100

    added editor config settings

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,29 +1,27 @@
-'use client'
+'use client';
 
-import { Comments as CommentsComponent } from 'pliny/comments'
-import { useRef, useState, useEffect } from 'react'
-import siteMetadata from '@/data/siteMetadata'
-import { useIntersectionObserver } from 'usehooks-ts'
+import { Comments as CommentsComponent } from 'pliny/comments';
+import { useRef, useState, useEffect } from 'react';
+import siteMetadata from '@/data/siteMetadata';
+import { useIntersectionObserver } from 'usehooks-ts';
 
 export default function Comments({ slug }: { slug: string }) {
-  const [loadComments, setLoadComments] = useState(false)
+	const [loadComments, setLoadComments] = useState(false);
 
-  const ref = useRef<HTMLDivElement | null>(null)
-  const entry = useIntersectionObserver(ref, {})
-  const isVisible = !!entry?.isIntersecting
+	const ref = useRef<HTMLDivElement | null>(null);
+	const entry = useIntersectionObserver(ref, {});
+	const isVisible = !!entry?.isIntersecting;
 
-  useEffect(() => {
-    if (isVisible) {
-      setLoadComments(true)
-    }
-  }, [isVisible])
+	useEffect(() => {
+		if (isVisible) {
+			setLoadComments(true);
+		}
+	}, [isVisible]);
 
-  return (
-    <div ref={ref}>
-      {!loadComments && <button onClick={() => setLoadComments(true)}>Load Comments</button>}
-      {siteMetadata.comments && loadComments && (
-        <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
-      )}
-    </div>
-  )
+	return (
+		<div ref={ref}>
+			{!loadComments && <button onClick={() => setLoadComments(true)}>Load Comments</button>}
+			{siteMetadata.comments && loadComments && <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />}
+		</div>
+	);
 }
