e6429ecc4a8a8c645cc53aaaa5c571c7dcf73b78
Matt Wicks <matt.wicks@gmail.com>
2026-03-05 12:20:06 +1100

    Refactor post filtering logic and apply it across blog archive pages

--- src/utils/postFilter.ts
+++ src/utils/postFilter.ts
@@ -5,7 +5,9 @@ const postFilter = ({ data }: CollectionEntry<"blog">) => {
 	const isPublishTimePassed =
 		Date.now() >
 		new Date(data.pubDatetime).getTime() - SITE.scheduledPostMargin;
-	return !data.draft && (import.meta.env.DEV || isPublishTimePassed);
+	const isDraft = data.draft;
+	if (isDraft && !import.meta.env.DEV) return false;
+	return import.meta.env.DEV || isPublishTimePassed;
 };
 
 export default postFilter;
